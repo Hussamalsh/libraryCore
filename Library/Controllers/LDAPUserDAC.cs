b8a1337ae5a714bb4539a851a95a34e761247bb7
Hussam <husals14@student.hh.se>
2017-12-03 17:35:45 +0100

    Add project files.

---
+++ Library/Controllers/LDAPUserDAC.cs
@@ -0,0 +1,102 @@
+﻿using RPL.VDB.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.DirectoryServices;
+using System.Linq;
+using System.Text;
+
+namespace Library
+{
+    public static class LDAPUserDAC
+    {
+        // ref: http://www.codeproject.com/Tips/599697/Get-list-of-Active-Directory-users-in-Csharp
+        public static List<LDAPUser> GetUsersFromActiveDirectory(string domainPath, out string message)
+        {
+            try
+            {
+                List<LDAPUser> aDUsers = new List<LDAPUser>();
+                DirectoryEntry searchRoot = new DirectoryEntry(domainPath);
+                DirectorySearcher search = new DirectorySearcher(searchRoot);
+                search.Filter = "(&(objectClass=user)(objectCategory=person))";
+                search.PropertiesToLoad.Add("samaccountname");
+                search.PropertiesToLoad.Add("mail");
+                search.PropertiesToLoad.Add("displayname");
+                SearchResult result;
+                SearchResultCollection resultCol = search.FindAll();
+                if (resultCol != null)
+                {
+                    for (int counter = 0; counter < resultCol.Count; counter++)
+                    {
+                        result = resultCol[counter];
+                        if (result.Properties.Contains("samaccountname")
+                            && result.Properties.Contains("mail")
+                            && result.Properties.Contains("displayname"))
+                        {
+                            LDAPUser user = new LDAPUser();
+                            user.UserName = (String)result.Properties["samaccountname"][0];
+                            user.DisplayName = (String)result.Properties["displayname"][0];
+                            user.Email = !string.IsNullOrWhiteSpace((String)result.Properties["mail"][0]) ? (String)result.Properties["mail"][0]
+                                : string.Empty;
+                            aDUsers.Add(user);
+                        }
+                    }
+                }
+                message = "OK";
+                return aDUsers;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return null;
+            }
+        }
+
+        public static LDAPUser GetUserFromActiveDirectory(string username, string domainPath, out string message)
+        {
+            try
+            {
+                LDAPUser adUser = new LDAPUser();
+                DirectoryEntry searchRoot = new DirectoryEntry(domainPath);
+                DirectorySearcher search = new DirectorySearcher(searchRoot);
+                search.Filter = "(&(objectClass=user)(objectCategory=person))";
+                search.PropertiesToLoad.Add("samaccountname");
+                search.PropertiesToLoad.Add("mail");
+                search.PropertiesToLoad.Add("displayname");
+                SearchResult result;
+                SearchResultCollection resultCol = search.FindAll();
+                if (resultCol != null)
+                {
+                    for (int counter = 0; counter < resultCol.Count; counter++)
+                    {
+                        result = resultCol[counter];
+                        if (result.Properties.Contains("samaccountname")
+                            && result.Properties.Contains("mail")
+                            && result.Properties.Contains("displayname"))
+                        {
+                            string smaName = (String)result.Properties["samaccountname"][0];
+                            if (!string.IsNullOrWhiteSpace(smaName)
+                                && smaName.Trim().ToLower() == username.Trim().ToLower())
+                            {
+                                adUser = new LDAPUser();
+                                adUser.UserName = (String)result.Properties["samaccountname"][0];
+                                adUser.DisplayName = (String)result.Properties["displayname"][0];
+                                adUser.Email = !string.IsNullOrWhiteSpace((String)result.Properties["mail"][0]) ? (String)result.Properties["mail"][0]
+                                    : string.Empty;
+                                break;
+                            }
+                        }
+                    }
+                }
+                message = "OK";
+                return adUser;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return null;
+            }
+        }
+    }
+}
---
+++ Library/Models/VDBUser.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPL.VDB.Model
+{
+    public static class User_Initials
+    {
+        public static string Get_Initials_From_Full_Name(string Full_Name)
+        {
+            string initials = string.Empty;
+            if (!string.IsNullOrWhiteSpace(Full_Name))
+            {
+                try
+                {
+                    Full_Name = Full_Name.Trim();
+                    if (Full_Name.ToUpper() == "Doctors".ToUpper()
+                        || Full_Name.ToUpper() == "Doctor".ToUpper())
+                    {
+                        return "DRs";
+                    }
+                    else
+                        if (Full_Name.ToUpper() == "Research Physicians".ToUpper()
+                            || Full_Name.ToUpper() == "Research Physician".ToUpper())
+                    {
+                        return "RPs";
+                    }
+                    else
+                        if (Full_Name.ToUpper() == "CTA"
+                            || Full_Name.ToUpper() == "Clinical Staff".ToUpper()
+                            || Full_Name.ToUpper() == "Clinical Trial Assistant".ToUpper())
+                    {
+                        return "CTa";
+                    }
+                    else
+                        if (Full_Name.ToUpper() == "CQm"
+                            || Full_Name.ToUpper() == "Quality Manager".ToUpper()
+                            || Full_Name.ToUpper() == "Clinical Quality Manager".ToUpper())
+                    {
+                        return "CQm";
+                    }
+                    else
+                        if (Full_Name.ToUpper() == "VRs"
+                            || Full_Name.ToUpper() == "Volunteer Recruitments"
+                            || Full_Name.ToUpper() == "Volunteer Recruitment")
+                    {
+                        return "VRs";
+                    }
+
+                    var names_by_space = Full_Name.Trim().Split(' ');
+                    if (names_by_space != null && names_by_space.Length > 0)
+                    {
+                        // remove empty
+                        names_by_space = names_by_space.Where(t => !string.IsNullOrWhiteSpace(t)).ToArray();
+
+                        if (names_by_space.Length > 3)
+                        {
+                            // First Character
+                            initials = string.IsNullOrWhiteSpace(names_by_space[0].Trim()) ? string.Empty
+                            : names_by_space[0].Trim().Substring(0, 1).ToUpper();
+                            // Second Character
+                            initials += string.IsNullOrWhiteSpace(names_by_space[1].Trim()) ? string.Empty
+                                : names_by_space[1].Trim().Substring(0, 1).ToUpper();
+                            // Last Character
+                            initials += string.IsNullOrWhiteSpace(names_by_space[names_by_space.Length - 1].Trim()) ? string.Empty
+                                : names_by_space[names_by_space.Length - 1].Trim().Substring(0, 1).ToUpper();
+                        }
+                        else
+                        {
+                            foreach (var name in names_by_space)
+                            {
+                                var names_by_dash = name.Trim().Split('-');
+                                if (names_by_dash.Length > 1)
+                                {
+                                    // remove empty
+                                    names_by_dash = names_by_dash.Where(n => !string.IsNullOrWhiteSpace(n)).ToArray();
+
+                                    initials += string.IsNullOrWhiteSpace(names_by_dash[0].Trim()) ? string.Empty
+                                        : names_by_dash[0].Trim().Substring(0, 1).ToUpper();
+                                    initials += string.IsNullOrWhiteSpace(names_by_dash[1].Trim()) ? string.Empty
+                                        : names_by_dash[1].Trim().Substring(0, 1).ToUpper();
+                                }
+                                else
+                                {
+                                    initials += string.IsNullOrWhiteSpace(name.Trim()) ? string.Empty
+                                        : name.Trim().Substring(0, 1).ToUpper();
+                                }
+                            }
+                            if (initials.Length > 3)
+                            {
+                                initials =
+                                    // First Character
+                                    initials.Substring(0, 1)
+                                    // Second Last Character
+                                    + initials.Substring(initials.Length - 2, 1)
+                                    // Last Character
+                                    + initials.Substring(initials.Length - 1, 1);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        initials = Full_Name.Substring(0, 1).ToUpper();
+                    }
+                }
+                catch (Exception) { }
+            }
+            return initials;
+        }
+    }
+
+
+
+
+    [Serializable]
+    public class LDAPUser
+    {
+        public string UserName { get; set; }
+        public string DisplayName { get; set; }
+        public string Initials { get; set; }
+        public string Email { get; set; }
+
+        public LDAPUser() { }
+    }
+}
